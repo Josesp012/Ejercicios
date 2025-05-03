ec474b472e8358173f4ab3a58ead8b9e23c573c4
Josesp012 <jose.salazarp_9612@hotmail.com>
2025-05-03 01:18:34 +0000

    edicion numeros primos descomposicion

--- C/descomposicion.c
+++ C/descomposicion.c
@@ -1,10 +1,32 @@
 //Descomposicion de factores primos de un numero
 
 /*
+2,3,4,5,6,7,8
 Sucesion numeros primos
-2,5,7,11
+2,3,5,7,11
 1. Numero, iteracion=1
 
 si i%2==0
 contar 
 */
+#include <stdio.h>
+int main(){
+    int n=6, c=1;
+    for(int i=2; i<n;i++){
+  	if(n%i==0){
+    n/=i;
+    printf("%d %d \n",i,n);
+    }
+    }
+    /*
+    if(c>2){
+  	    printf("No es primo");
+    }else{
+  	printf("%d",n);
+    }
+    */
+    
+    
+
+  return 0;
+}
--- C/numeros_primos.c
+++ C/numeros_primos.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main(){
-    int n=5, c=0;
-    for(int i=1; i<=n;i++){
+    int n=1, c=0;
+    for(int i=2; i<=n;i++){
   	if(n%i==0){
     c++;
     }
     }
-    if(c>2){
+    if(c>2 || n==1){
   	    printf("No es primo");
     }else{
   	printf("Es primo");
