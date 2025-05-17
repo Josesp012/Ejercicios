d6f3b5a3d170d1e87ca586aa57116760a2f3c964
Josesp012 <jose.salazarp_9612@hotmail.com>
2025-05-17 06:09:12 +0000

    ejercicio diagonal completo

--- C++/matrizDiagonal.cpp
+++ C++/matrizDiagonal.cpp
@@ -5,11 +5,29 @@ int main() {
   string diagonal[4][4];
   
   int filas=3,columnas=3;
-  
-  for (int i = 0; i < filas; i++) {
+/*cuadrado
+for (int i = 0; i < filas; i++) {
   	for (int j = 0; j < columnas; j++) {
-      diagonal[i][j]="/";
-      cout<<diagonal[i][j];
+      if(j==0||i==0||i==columnas-1||j==filas-1){
+        diagonal[i][j]="=";
+        cout<<diagonal[i][j];
+      }
+      else{
+      	diagonal[i][j]=" ";
+        cout<<diagonal[i][j];
+      }
+	}
+*/
+ for (int i = 0; i < filas; i++) {
+  	for (int j = 0; j < columnas; j++) {
+      if(i==0&&j==filas-1||i==filas/2&&j==columnas/2||i==filas-1&&j==0){
+        diagonal[i][j]="/";
+        cout<<diagonal[i][j];
+      }
+      else{
+      	diagonal[i][j]=" ";
+        cout<<diagonal[i][j];
+      }
 	}
     cout<<"\n";
   }
